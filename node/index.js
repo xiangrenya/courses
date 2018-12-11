2a63aef94a5f7eebffd092de05e64429b5b41e6e
allen.xiang <allen.xiang@gaiaworks.cn>
2018-12-11 15:22:07 +0800

    异常处理

--- node/index.js
+++ node/index.js
@@ -11,11 +11,26 @@ require('./models/connection');
 const app = new Koa();
 app.use(serve(path.join(__dirname, '/public')));
 app.use(logger());
+
+// error handler
+app.use(async function(ctx, next) {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.body = {
+      result: false,
+      message: '服务器发生了错误, 请联系管理员邮箱: xiangrenya@gmail.com'
+    };
+    ctx.app.emit('error', err, ctx);
+  }
+});
+
 app.use(
   koaBody({
     multipart: true,
     formidable: {
-      maxFileSize: 2 * 1024 * 1024, // 设置上传文件大小最大限制，默认2M
+      maxFileSize: 20 * 1024 * 1024, // 设置上传文件大小最大限制，默认2M
       uploadDir: 'upload/',
       onFileBegin: (name, file) => {
         // 可以通过 input 属性 name，来区分文件存放路径，如 images,docs 等等
@@ -31,7 +46,18 @@ app.use(
 app.use(router.routes()).use(router.allowedMethods());
 
 app.use(async function(ctx, next) {
-  ctx.redirect('/404.html');
+  ctx.status = 404;
+  ctx.body = {
+    result: false,
+    message: '接口地址未找到'
+  };
+});
+
+app.on('error', function(err) {
+  if (process.env.NODE_ENV != 'test') {
+    console.log('sent error %s to the cloud', err.message);
+    console.log(err);
+  }
 });
 
 app.listen(3000, err => {
